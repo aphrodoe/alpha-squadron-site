8d79e937afb93981913c5d97a21a698c2a13264f
Akhil Dhyani <akhildhyani420@gmail.com>
2025-11-23 21:04:23 +0530

    m

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='text-center bg-[#171c21] text-white p-4'>
         Copyright © Society of Alumni Affairs, IIT Jodhpur<br/>
-        Website designed by Akhil Dhyani
+        Website designed by team at <a href="https://ignedia.tech" className='underline'>Ignedia.tech</a>
     </div>
   )
 }
